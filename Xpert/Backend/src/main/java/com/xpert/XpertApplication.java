6f9a82c54c7ef43b151a0f5b26c087c3cf102fbd
Ashik <nasirhasan.ashik@gmail.com>
2025-04-13 16:25:33 +0600

    xpertApplication.java updated

--- Xpert/Backend/src/main/java/com/xpert/XpertApplication.java
+++ Xpert/Backend/src/main/java/com/xpert/XpertApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class XpertApplication {
 
 	public static void main(String[] args) {
-		 SpringApplication.run(XpertApplication.class, args);
-		
-	
+		SpringApplication.run(XpertApplication.class, args);
     }
 
 }
