c841dfe41fd949c66a5b191f2549b6917660fc54
Ashik <nasirhasan.ashik@gmail.com>
2025-04-13 16:14:01 +0600

    XpertApplication.java updated

--- Xpert/Backend/src/main/java/com/xpert/XpertApplication.java
+++ Xpert/Backend/src/main/java/com/xpert/XpertApplication.java
@@ -2,26 +2,14 @@ package com.xpert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import com.xpert.repository.UserRepository;
 
 @SpringBootApplication
 public class XpertApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context =  SpringApplication.run(XpertApplication.class, args);
+		 SpringApplication.run(XpertApplication.class, args);
 		
-		UserRepository userRepository = context.getBean(UserRepository.class);
 	
-		userRepository.findAll().forEach(user ->
-	    System.out.println("User: " + user.getFirstName() + " " + user.getLastName() + ", ID: " + user.getId())
-	);
-
-
-	
-
     }
-		
-	
 
 }
