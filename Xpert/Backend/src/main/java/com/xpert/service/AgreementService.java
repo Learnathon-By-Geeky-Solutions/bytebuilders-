18f9ebdaaf61d66ef0fec85c98fafa4b5317ae0a
Ashik <nasirhasan.ashik@gmail.com>
2025-04-16 22:32:12 +0600

    AgreementService Updated

--- Xpert/Backend/src/main/java/com/xpert/service/AgreementService.java
+++ Xpert/Backend/src/main/java/com/xpert/service/AgreementService.java
@@ -17,6 +17,8 @@ public interface AgreementService {
      *
      * @param dto the request data containing agreement details
      * @return the created agreement as a response DTO
+     *  @throws IllegalArgumentException if agreement data is invalid
+     *  @throws EntityExistsException if agreement already exists
      */
     AgreementResponseDTO createAgreement(CreateAgreementRequestDTO dto);
 
@@ -25,6 +27,7 @@ public interface AgreementService {
      *
      * @param agreementId the unique identifier of the agreement
      * @return the corresponding agreement response DTO
+     * @throws EntityNotFoundException if no agreement exists with the given ID
      */
     AgreementResponseDTO getAgreementById(UUID agreementId);
 
