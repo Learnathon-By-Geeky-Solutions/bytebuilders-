19f54b365cc7416244a7aa3a310c7ee95d2da1ac
Ashik <nasirhasan.ashik@gmail.com>
2025-04-17 15:02:45 +0600

    CreateLLocationRequestDTo Updated

--- Xpert/Backend/src/main/java/com/xpert/dto/location/CreateLocationRequestDTO.java
+++ Xpert/Backend/src/main/java/com/xpert/dto/location/CreateLocationRequestDTO.java
@@ -14,7 +14,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @NoArgsConstructor
-@ToString
+@ToString(exclude = {"streetAddress", "specialInstruction"})
 public class CreateLocationRequestDTO {
 
     /**
