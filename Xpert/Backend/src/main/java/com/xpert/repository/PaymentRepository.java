3dfbeb13ccb1f1ee9550f658cf2993dbc8b01b4a
Ashik <nasirhasan.ashik@gmail.com>
2025-04-18 12:24:30 +0600

    CreatePaymentRequestDTO created

--- Xpert/Backend/src/main/java/com/xpert/repository/PaymentRepository.java
+++ Xpert/Backend/src/main/java/com/xpert/repository/PaymentRepository.java
@@ -1,7 +1,10 @@
 package com.xpert.repository;
 
 import com.xpert.entity.Payment;
+
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+public interface PaymentRepository extends JpaRepository<Payment, UUID> {
 }
